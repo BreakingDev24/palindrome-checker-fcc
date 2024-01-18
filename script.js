41680afab69a5068984b8ec27ce85c25b83c0f72
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-01-18 14:15:43 +0100

    get user input

---
+++ script.js
@@ -0,0 +1,34 @@
+// varibles
+
+const textInput = document.getElementById('text-input');
+const checkBtn = document.getElementById('check-btn');
+const result = document.getElementById('result');
+
+let userInput = ""
+
+function getUserInput () {
+    if(textInput.value == ""){
+        alert('write something')
+    } else {
+        userInput = textInput.value
+        textInput.value = ""
+        console.log(userInput)
+    }
+    reverseUserInput()
+    renderResult(userInput)
+}
+
+
+function reverseUserInput (){
+    
+    let reverseInput = userInput.split("").reverse().join("").replace(/[^0-9a-z]/gi, '')//reverse and remove not alphanumeric charcters
+    console.log(reverseInput);
+}
+
+function renderResult (input){
+    result.textContent = input
+}
+
+
+
+checkBtn.addEventListener("click", getUserInput)
